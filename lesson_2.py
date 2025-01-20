c2995261ed2f88262d17742aed1202b4669a0cc2
student <f2l2rrr@gmail.com>
2025-01-20 21:34:29 +0300

    lesson_2

--- lesson_2.py
+++ lesson_2.py
@@ -1,1 +1,36 @@
-names  = []
+# names = []
+#
+# # цикл For
+#
+# for i in names:
+#     print(i)
+# #range(6) = (0,1,2,3,4,5)
+# #range(1,5) = (1,2,3,4)
+#
+# for i in range(len(names)):
+#     print(names[i])
+#
+# persone = {name}
+# name = [Roman, age = '2']
+# name = info["name"]
+#
+# print(info["name"])
+#
+# info["phone"] = "89112389114"
+# info["name"] = "Roman"
+#
+# print(
+
+#
+# users = []
+# info_persone = {}
+#
+# for i in range(3):
+#     name = input("Enter the name")
+#     phone = input("Enter the phone")
+#     info_persone = {"name": name, "phone": phone}
+#     users.append(info_persone)
+#
+# print
+
+print(list(info.case()))
